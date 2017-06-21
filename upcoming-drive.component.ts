d2d6af1d470a571a97a5c6609caa29c83a7ae473
Khushboo21417 <khushboo.21417@lpu.co.in>
2017-06-21 23:20:39 +0530

    Add files via upload

---
+++ upcoming-drive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-drive',
+  templateUrl: './upcoming-drive.component.html',
+  styleUrls: ['./upcoming-drive.component.css']
+})
+export class UpcomingDriveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
