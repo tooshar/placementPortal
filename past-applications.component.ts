cfdbaa9f873c8c8b015aeb0654e38167ff0f5cb4
isha21416 <isha.21416@lpu.co.in>
2017-06-22 09:15:22 +0530

    Add files via upload

---
+++ past-applications.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-past-applications',
+  templateUrl: './past-applications.component.html',
+  styleUrls: ['./past-applications.component.css']
+})
+export class PastApplicationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("in past applications");
+  }
+
+}
