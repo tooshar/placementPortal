cd47d71f6a5cbd23808ffd1f23a1f81e6e28a1cb
lakhankadam1 <lakhankadam1@gmail.com>
2017-06-22 12:16:34 +0530

    Add files via upload

---
+++ invite-companies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-companies',
+  templateUrl: './invite-companies.component.html',
+  styleUrls: ['./invite-companies.component.css']
+})
+export class InviteCompaniesComponent implements OnInit {
+  swapped: Boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
