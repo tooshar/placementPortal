da3662e438908475118a0beceabe000eed77ee13
ashutosh21414 <ashutosh.21414@lpu.co.in>
2017-06-22 09:23:22 +0530

    Add files via upload

---
+++ university-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-university-profile',
+  templateUrl: './university-profile.component.html',
+  styleUrls: ['./university-profile.component.css']
+})
+export class UniversityProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
