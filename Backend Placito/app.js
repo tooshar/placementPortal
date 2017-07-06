2b289d7a53748b56e90b3fc4c14aed6946e90643
ashutosh21414 <ashutosh.21414@lpu.co.in>
2017-07-06 12:58:44 +0530

    Not to be deleted

--- Backend Placito/app.js
+++ Backend Placito/app.js
@@ -5,22 +5,26 @@ const path = require("path");
 const student_router = require('./Routes/student/student_route');
 const university_router = require('./Routes/university/university_route');
 
-
+const mongoose = require('mongoose');		
+ const bodyParser = require('body-parser');		
+  
 
 //Intializing app 
 const app = express();
 
-//Definiing routes 
+mongoose.connect("mongodb://ats:123456789a@ds127730.mlab.com:27730/university_profile");		
+ mongoose.Promise = global.Promise;
 
+//Definiing routes 
+app.use(bodyParser.urlencoded({extended: false}));		
+app.use(bodyParser.json());
 app.use('/university', university_router);
 app.use('/student', student_router);
 
+//Intializing the port address
 
-//Intializing
 const port  = 3000;
-
-app.listen(3000)
-{
-   console.log("Server Started @ 3000");
-    
-}
+app.listen(port)
+  {		  
+    console.log("Server Started @ "+port);	     
+  }
